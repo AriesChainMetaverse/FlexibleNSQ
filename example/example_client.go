dd4d69d22effe8c16904a5a51c37fd83e07b688c
godcong <jumbycc@163.com>
2021-06-03 11:06:55 +0800

    add example

---
+++ example/example_client.go
@@ -0,0 +1,48 @@
+package example
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	fnsq "github.com/DragonveinChain/FlexibleNSQ"
+)
+
+func Client() {
+	config := fnsq.DefaultConfig()
+	config.ConsumeAddr = "192.168.2.201:4161"
+	config.ProducerAddr = "192.168.2.201:4150"
+	manage := fnsq.NewManager(context.TODO(), config)
+	str := "hello world"
+	manage.Start()
+	go func() {
+		//stop in other process
+		time.Sleep(1000 * time.Second)
+		manage.Stop()
+	}()
+
+	for i := 0; i < 100; i++ {
+		manage.RegisterClient("client1", fnsq.WorkMessage{
+			Topic:  "rnd" + strconv.Itoa(i),
+			Length: len(str),
+			Data:   []byte(str),
+		})
+	}
+
+	go func() {
+		works := manage.Works()
+		//for {
+		for i := range works {
+			msg := <-works[i].Message()
+			message, err := fnsq.ParseMessage(msg.Body)
+			if err != nil {
+				return
+			}
+			fmt.Println(message.Topic, message, "data", string(message.Data))
+		}
+		//}
+	}()
+
+	manage.Wait()
+}
---
+++ example/example_server.go
@@ -0,0 +1,38 @@
+package example
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	fnsq "github.com/DragonveinChain/FlexibleNSQ"
+)
+
+func Server() {
+	config := fnsq.DefaultConfig()
+	config.ConsumeAddr = "192.168.2.201:4161"
+	config.ProducerAddr = "192.168.2.201:4150"
+	manage := fnsq.NewManager(context.TODO(), config)
+
+	manage.Start()
+	go func() {
+		//stop in other process
+		time.Sleep(1000 * time.Second)
+		manage.Stop()
+	}()
+	work := manage.StartRegisterServer("server1")
+	go func() {
+		for {
+			msg := <-work.Message()
+			message, err := fnsq.ParseMessage(msg.Body)
+			if err != nil {
+				return
+			}
+			fmt.Println("msg", message, "data", string(message.Data))
+			str := "hello world server"
+			manage.PublishWork(message.Work([]byte(str), 0))
+		}
+	}()
+
+	manage.Wait()
+}
